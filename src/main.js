3bdcda7494a8263e9d5aba24fe31699ad6f2d345
José Diaz <xoxefdp@gmail.com>
2020-02-24 22:55:26 +0100

    Exported ConnectionState

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import { CONNECTED, DISCONNECTED } from 'Constants/States'
 import { ON_NETWORK_CHECKING, ON_NETWORK_CHANGED, ON_NETWORK_CONNECTED, ON_NETWORK_DISCONNECTED } from 'Constants/Events'
 import { connectionState, startConnectionChecker, stopConnectionChecker, checkConnectionOnDemand } from 'Checker'
 
+exports.ConnectionState = {
+  CONNECTED,
+  DISCONNECTED
+}
 exports.ConnectionEvent = {
   ON_NETWORK_CHECKING,
   ON_NETWORK_CHANGED,
