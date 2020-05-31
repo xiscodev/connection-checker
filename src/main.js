13c8c3d90afaff07b07600a2c2b035dd471023e0
José Diaz <xoxefdp@gmail.com>
2020-05-31 21:42:53 +0200

    Implemented changeTimeout and changeInterval to improve service customization

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import ConnectionState from 'Constants/States'
 import ConnectionEvent from 'Constants/Events'
-import { getConnectionState, isCheckerActive, startChecker, stopChecker, checkOnDemand } from 'Checker'
+import { getConnectionState, changeTimeout, changeInterval, isCheckerActive, startChecker, stopChecker, checkOnDemand } from 'Checker'
 
 exports.ConnectionState = ConnectionState
 exports.ConnectionEvent = ConnectionEvent
 exports.getConnectionState = getConnectionState
+exports.changeTimeout = changeTimeout
+exports.changeInterval = changeInterval
 exports.isCheckerActive = isCheckerActive
 exports.startChecker = startChecker
 exports.stopChecker = stopChecker
